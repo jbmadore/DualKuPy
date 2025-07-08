9356f966fb00904fcb7620d846f64a17d0e30ce3
Nico <nicolasallet4@gmail.com>
2025-07-08 14:00:19 -0400

    je sais meme pas honnetement

--- data_io/file_writer.py
+++ data_io/file_writer.py
@@ -89,7 +89,7 @@ def write_chirp_to_file(file, chirp_number, timestamp, data):
     file.write("# --- End of Chirp ---\n\n")
 
 
-def record_measurement(num_records=50, foldername="./data/", measure_number=1, options=None, fullname=None):
+def record_measurement(num_records=50, foldername="/home/nico/DualKuPy/data/", measure_number=1, options=None, fullname=None):
     """
     Records a measurement consisting of `num_records` chirps, writing both header and chirp data.
     
--- launchcontinuousmeas.sh
+++ launchcontinuousmeas.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Chemin vers le script Python
-SCRIPT_PYTHON="./radar_continuous.py"
-
+SCRIPT_PYTHON="/home/nico/DualKuPy/radar_continuous.py"
+config_file="/home/nico/DualKuPy/config/config_bonzai_jan_15.json"
 # Exécution du script Python
-python3 $SCRIPT_PYTHON
+python3 $SCRIPT_PYTHON $config_file
 
 # Mettre l'ordinateur en veille pendant 15 minutes (900 secondes)
 rtcwake -m mem -s 900
